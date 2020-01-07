5df81c0da912fb74c2f6aa590d863fbe3e9bf60a
Jon <grebej@uwindsor.ca>
2020-01-06 20:14:22 -0500

    updates to pi image taking script

--- image_processing/read_serial.py
+++ image_processing/read_serial.py
@@ -1,6 +1,34 @@
 #!/usr/bin/env python3
 import time
 import serial
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+from datetime import datetime
+
+def take_picture():
+    # initialize the camera and grab a reference to the raw camera capture
+    camera = PiCamera()
+    rawCapture = PiRGBArray(camera)
+
+    # allow the camera to warmup
+    time.sleep(0.1)
+
+    # grab an image from the camera
+    camera.capture(rawCapture, format="bgr")
+    image = rawCapture.array
+
+    # display the image on screen and wait for a keypress
+    #cv2.imshow("Image", image)
+    #cv2.waitKey(0)
+
+    # Converting datetime object to string
+    dateTime = datetime.now()
+    timestamp = dateTime.strftime("%d-%b-%Y (%H:%M:%S.%f)")
+
+    # save image with timestamp
+    cv2.imwrite(timestamp + '.png', image)
 
 ser = serial.Serial(
         port='/dev/ttyAMA0',
@@ -17,3 +45,5 @@ while 1:
     x = ser.readline();
     if x != '':
         print(x);
+    if x == '1':
+        take_picture()
